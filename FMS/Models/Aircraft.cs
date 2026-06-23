15caeff73944f861c825dbe3bb486af6bb4ea92d
Ahmed <alshukairi1234@gmail.com>
2026-06-23 16:00:05 +0400

    created Aircraft.cs File

---
+++ FMS/Models/Aircraft.cs
@@ -0,0 +1,9 @@
+namespace FMS;
+
+public class Aircraft
+{
+    public int aircraftID { get; set; }
+    public string model { get; set; }
+    public int totalSeats { get; set; }
+    public bool isOperational { get; set; }
+}
