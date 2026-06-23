92de1f6084e42210143a110f90fa5837264c7379
Ahmed <alshukairi1234@gmail.com>
2026-06-23 17:22:15 +0400

    created the Context Files

--- FMS/FMSContext.cs
+++ FMS/FMSContext.cs
@@ -2,5 +2,9 @@ namespace FMS;
 
 public class FMSContext
 {
-    
+    public List<Aircraft>  aircrafts { get; set; }
+    public List<Flight> flights { get; set; }
+    public List<Pilot> pilots { get; set; }
+    public List<Passanger> passangers { get; set; }
+    public List<Booking>  bookings { get; set; }
 }
