900d8c540965fea1222edf45410c4ad948466ad4
Ahmed <alshukairi1234@gmail.com>
2026-06-28 16:05:07 +0400

    refactored the tagline in Aircraft.cs

--- FMS/Models/Aircraft.cs
+++ FMS/Models/Aircraft.cs
@@ -2,8 +2,15 @@ namespace FMS;
 
 public class Aircraft
 {
+    // System Generated
     public int aircraftID { get; set; }
+
+    // User Input
     public string model { get; set; }
+
+    // System Calculated
     public int totalSeats { get; set; }
+
+    // Default Value
     public bool isOperational { get; set; }
 }
