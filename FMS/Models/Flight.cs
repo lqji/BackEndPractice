c7330576bc60855471807227e395370cbd3037be
Ahmed <alshukairi1234@gmail.com>
2026-06-28 16:05:35 +0400

    refactored the tagline in Flight.cs.cs

--- FMS/Models/Flight.cs
+++ FMS/Models/Flight.cs
@@ -1,19 +1,23 @@
 namespace FMS;
-
 public class Flight
 {
-    public string flightNumber { get; set; }
-    public int flightId { get; set; } 
-    public string flightStatus { get; set; }
-    public string flightCode { get; set; }
-    public int aircraftId { get; set; }
-    public int pilotId { get; set; }
-    public string origin { get; set; }
-    public string destination { get; set; }
-    public DateTime departureDate { get; set; }
-    public DateTime departureTime { get; set; }
-    public double ticketPrice { get; set; }
-    public double availableSeats { get; set; }
-    
-    
+    // System Generated
+    public int flightId { get; set; }           
+    public string flightCode { get; set; }       
+    public string flightNumber { get; set; }     
+
+    // User Input
+    public int aircraftId { get; set; }          
+    public int pilotId { get; set; }             
+    public string origin { get; set; }           
+    public string destination { get; set; }      
+    public DateTime departureDate { get; set; }  
+    public DateTime departureTime { get; set; }  
+    public double ticketPrice { get; set; }      
+
+    // System Copied 
+    public double availableSeats { get; set; }  
+
+    // Defult Value
+    public string flightStatus { get; set; }     
 }
