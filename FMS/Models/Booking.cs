5708c340bd42d5e1e58d903b9d3eeb6eee6b1fec
Ahmed <alshukairi1234@gmail.com>
2026-06-28 16:05:17 +0400

    refactored the tagline in Booking.cs

--- FMS/Models/Booking.cs
+++ FMS/Models/Booking.cs
@@ -2,12 +2,22 @@ namespace FMS;
 
 public class Booking
 {
-    public int BookingId { get; set; }
-    public int passengerId { get; set; }
-    public int flightId { get; set; }
-    public string seatNumber { get; set; }
-    public string bookingDate { get; set; }
-    public double totalPrice { get; set; }
-    public string status { get; set; }
-    
+    // System Generated
+    public int BookingId { get; set; }          
+
+    // User Input
+    public int passengerId { get; set; }        
+    public int flightId { get; set; }           
+
+    // System Calculated 
+    public string seatNumber { get; set; }      
+
+    // System Generated
+    public string bookingDate { get; set; }     
+
+    // System Copied
+    public double totalPrice { get; set; }      
+
+    // Defult Value
+    public string status { get; set; }          
 }
