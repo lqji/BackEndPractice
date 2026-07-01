d26d26a130acba296061618ef15c0e7ec01f9808
Ahmed <alshukairi1234@gmail.com>
2026-07-01 18:17:09 +0400

    implemented Student.cs with constraints and relations

--- UnivisitySystem/models/Student.cs
+++ UnivisitySystem/models/Student.cs
@@ -1,9 +1,41 @@
 using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace UnivisitySystem.models;
-[Index(nameof(Student), IsUnique = true)]
+
+
 public class Student
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int studentId { get; set; } // system generated
+
+    [Required]
+    [MaxLength(100)]
+    public string fullName { get; set; } // user input
+
+    [Required]
+    [MaxLength(150)]
+    public string email { get; set; } // user input
+
+    [MaxLength(20)]
+    public string phoneNumber { get; set; } // user input
+
+    [Required]
+    public DateTime dateOfBirth { get; set; } // user input
+
+    [Required]
+    [Range(2000, 2030)]
+    public int enrollmentYear { get; set; } // user input
+
+    [Range(0.0, 4.0)]
+    public decimal gpa { get; set; } = 0.0m; // default value / calculated
+
     
+    public ICollection<Enrollment> Enrollments { get; set; } // system generated
 }
