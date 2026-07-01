5de8fe467e960b02c1555631579487884157ebb3
Ahmed <alshukairi1234@gmail.com>
2026-07-01 18:15:08 +0400

    implemented Department.cs with constraints and relations

--- UnivisitySystem/models/Department.cs
+++ UnivisitySystem/models/Department.cs
@@ -1,6 +1,33 @@
-namespace UnivisitySystem.models;
+using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Department
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int departmentId { get; set; } // system generated
+
+    [Required]
+    [MaxLength(100)]
+    public string departmentName { get; set; } // user input
+
+    [MaxLength(50)]
+    public string building { get; set; } // user input
+
+    [Required]
+    [Range(0, double.MaxValue, ErrorMessage = "Budget must be greater than or equal to 0")]
+    public decimal budget { get; set; } // user input
+
+    public int? headInstructorId { get; set; } // from list / user input
+
     
+    
+    [ForeignKey("headInstructorId")]
+    public Instructor HeadInstructor { get; set; } // system generated
+
+    public ICollection<Course> Courses { get; set; } // system generated
 }
