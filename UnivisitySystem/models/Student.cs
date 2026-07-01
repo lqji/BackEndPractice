24e824b410c06e02896f6b9e0a18bdbd3bf0e09a
Ahmed <alshukairi1234@gmail.com>
2026-07-01 17:46:57 +0400

    created student.cs class

---
+++ UnivisitySystem/models/Student.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnivisitySystem.models;
+[Index(nameof(Student), IsUnique = true)]
+public class Student
+{
+    
+}
