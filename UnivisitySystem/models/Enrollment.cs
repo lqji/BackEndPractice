a6c36a77634614b12a25b5648e4ca606a464a0bf
Ahmed <alshukairi1234@gmail.com>
2026-07-01 18:15:46 +0400

    implemented Enrollment.cs with constraints and relations

--- UnivisitySystem/models/Enrollment.cs
+++ UnivisitySystem/models/Enrollment.cs
@@ -1,6 +1,38 @@
-namespace UnivisitySystem.models;
+using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using UnivisitySystem.models;
 
 public class Enrollment
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int enrollmentId { get; set; } // system generated
+
+    [Required]
+    public int studentId { get; set; } // from list / user input
+
+    [Required]
+    public int courseId { get; set; } // from list / user input
+
+    [Required]
+    public DateTime enrollmentDate { get; set; } // system generated / calculated
+
+    [MaxLength(2)]
+    public string finalGrade { get; set; } // user input / calculated
+
+    [Required]
+    [MaxLength(20)]
+    public string status { get; set; } = "In Progress"; // default value
+
+
     
+    [ForeignKey("studentId")]
+    public Student Student { get; set; } // system generated
+
+    [ForeignKey("courseId")]
+    public Course Course { get; set; } // system generated
 }
