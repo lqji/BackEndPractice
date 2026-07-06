aa41add6dfc443e5dc500191c3f571c54bbb94a5
Ahmed <alshukairi1234@gmail.com>
2026-07-06 09:25:23 +0400

    refactored program file in Univesity System Project

--- UnivisitySystem/Program.cs
+++ UnivisitySystem/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace UnivisitySystem;
+﻿using System.Diagnostics.Contracts;
+using Microsoft.Data.SqlClient;
+
+namespace UnivisitySystem;
+using System;
+using UnivisitySystem.models;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
     }
+
 }
