10d5e792daa8664bab6255bce1c8d7f8936219c8
Ahmed <alshukairi1234@gmail.com>
2026-07-06 09:32:22 +0400

    finished setting up the ecommerceContext

--- EcommerceSystem/EcommerceContext.cs
+++ EcommerceSystem/EcommerceContext.cs
@@ -3,7 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceSystem;
 
-public class EcommerceContext 
+public class EcommerceContext : DbContext
 {
+    public DbSet<Category> _categories { get; set; }
+    public DbSet<Order> Orders { get; set; } 
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
     
 }
