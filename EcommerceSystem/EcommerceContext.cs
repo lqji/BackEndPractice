30249affb3489c1ec2dd5529b370ed4299c01425
Ahmed <alshukairi1234@gmail.com>
2026-07-07 12:34:23 +0400

    refactored the EcommerceContext.cs

--- EcommerceSystem/EcommerceContext.cs
+++ EcommerceSystem/EcommerceContext.cs
@@ -1,14 +1,30 @@
-using EcommerceSystem.Models;
 using Microsoft.EntityFrameworkCore;
+using EcommerceSystem.Models;
 
-namespace EcommerceSystem;
-
-public class EcommerceContext : DbContext
+namespace EcommerceSystem
 {
-    public DbSet<Category> _categories { get; set; }
-    public DbSet<Order> Orders { get; set; } 
-    public DbSet<OrderItem> OrderItems { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<User> Users { get; set; }
-    
+    public class ECommerceContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(@"Server=localhost;Database=ECommerceDB;User Id=sa;Password=Admin123;TrustServerCertificate=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderItem>()
+                .HasKey(oi => new { oi.OrderId, oi.ProductId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
