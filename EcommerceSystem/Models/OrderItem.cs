902aea0076a8be3d2b3838af7af87b18d7067d83
Ahmed <alshukairi1234@gmail.com>
2026-07-07 12:02:49 +0400

    finished setting up the OrderItem.cs class

--- EcommerceSystem/Models/OrderItem.cs
+++ EcommerceSystem/Models/OrderItem.cs
@@ -1,6 +1,25 @@
-namespace EcommerceSystem.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class OrderItem
+namespace EcommerceSystem.Models
 {
-    
+    public class OrderItem
+    {
+        [Required]
+        public int OrderId { get; set; } // foreign key
+
+        [Required]
+        public int ProductId { get; set; } // foreign key
+
+        [Required]
+        [Range(1, 999)]
+        public int Quantity { get; set; } // user input
+
+        // Navigation Properties
+        [ForeignKey("OrderId")]
+        public virtual Order Order { get; set; } // system generated
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; } // system generated
+    }
 }
