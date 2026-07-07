eefa351bf976a2493b43e0e5b82ba284d09dac8e
Ahmed <alshukairi1234@gmail.com>
2026-07-07 12:03:16 +0400

    finished setting up the Category.csry class

--- EcommerceSystem/Models/Category.cs
+++ EcommerceSystem/Models/Category.cs
@@ -1,6 +1,24 @@
-namespace EcommerceSystem.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-public class Category
+namespace EcommerceSystem.Models
 {
-    
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; } // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string CategoryName { get; set; } // user input
+
+        [MaxLength(500)]
+        public string Description { get; set; } // user input
+
+        [MaxLength(300)]
+        public string ImageUrl { get; set; } // user input
+
+        // Navigation Properties
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>(); // system generated
+    }
 }
